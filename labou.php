6f5506b3b3a9fc09af0af8b6d4d7f1c74c9d8f47
tabajara <tabajara@gmail.com>
2015-01-27 03:11:29 -0200

    implemented winning factor of 2:1 as a variation from labouchere.

--- labou.php
+++ labou.php
@@ -5,6 +5,10 @@
 var high_value ;
 var seq_interval ; 
 var spin = 0 ; 
+var total_wins = 0 ; 
+var total_losses = 0 ; 
+var winning_return = 1 ; 
+var last_sequence ;
 
 function do_sequence() { 
 	seq_interval = parseFloat( document.getElementById('sequence-interval').value ) ; 
@@ -29,35 +33,82 @@ function do_sequence() {
 }
 
 function win() { 
+	var winning_return = parseFloat( document.getElementById('winning-return').value ) ; 
 	var sequence = document.getElementById('sequence-stream').innerHTML ; 
 	var nb = sequence.split(', ') ; 
 	new_sequence = '' ; 
 	if( nb.length > 1 ) {
-		nb.splice(0, 1);
-		nb.splice((nb.length-2), 1);
-		new_sequence = nb.join(', ') ; 
+		if( winning_return == 1 ) { 
+			nb.splice(0, 1);
+			nb.splice((nb.length-2), 1);
+			new_sequence = nb.join(', ') ; 
+		}
+		if( winning_return == 2 ) { 
+			var last_seq = last_sequence.split(', ') ; 
+			for( var i = 0 ; i < last_seq.length ; i++ ) { 
+				for( var j in nb ) {
+				    if( nb[j] == last_seq[i] ) {
+				        nb.splice( j, 1 ) ;
+				        break;
+					}
+				}
+			}
+			new_sequence = nb.join(', ') ; 
+		}
 	}
 	document.getElementById('sequence-stream').innerHTML = new_sequence ; 
 	document.getElementById('next-bet').innerHTML = next_bet() ; 
-	spins() ; 
+
+	total_wins++ ; 
+	document.getElementById('total-wins').innerHTML = total_wins ; 
+
+	spins() ; 	
 }
 
 function lose() { 
 	var nextb = document.getElementById('next-bet') ; 
 	document.getElementById('sequence-stream').innerHTML += nextb.innerHTML + ', ' ; 
 	nextb.innerHTML = next_bet() ; 
+
+	total_losses++ ; 
+	document.getElementById('total-losses').innerHTML = total_losses ; 
+
 	spins() ; 
 }
 
 function next_bet() { 
+	var winning_return = parseFloat( document.getElementById('winning-return').value ) ; 
 	var sequence = document.getElementById('sequence-stream').innerHTML ;
 	var nb = sequence.split(', ') ; 
+	last_sequence = '' ; 
 	if( nb.length <= 2 ) { 
-		return nb[0] ; 
+		if( winning_return == 1 ) { 
+			return nb[0] ; 
+		} else if( winning_return == 2 ) { 
+			return parseFloat(nb[0]/2).toFixed(2) ; 
+		}
 	} else if( nb.length > 1 ) {
-		var first = parseFloat( nb[0] ) ; 
-		var last = parseFloat( nb[(nb.length-2)] ) ; 
-		return parseFloat(first+last).toFixed(2) ;
+		if( winning_return == 1 ) { 
+			var first = parseFloat( nb[0] ) ; 
+			var last = parseFloat( nb[(nb.length-2)] ) ; 
+			return parseFloat(first+last).toFixed(2) ;
+		} else if( winning_return == 2 ) { 
+			var total = 0 ; 
+			if( nb.length > 5 ) { // losses to recoup
+				i_start = nb.length - 5 ; 
+				i_stop = nb.length ; 
+			} else { 
+				i_start = 0 ; 
+				i_stop = nb.length ; 
+			}
+			for( var i = i_start ; i < i_stop ; i++ ) { 
+				if( nb[i] > 0 ) { 
+					total += parseFloat( nb[i] ) ; 
+					last_sequence += nb[i] + ', ' ; 
+				}
+			}
+			return Math.ceil(parseFloat(total/2)).toFixed(2) ; 
+		}
 	}
 	return 0 ; 
 }
@@ -80,7 +131,11 @@ function spins() {
 </tr>
 <tr>
 	<td>Define the arithmetic interval:</td>
-	<td><input type="text" id="sequence-interval" name="sequence_interval" onBlur="javascript:do_sequence()" value="0.2"></td>
+	<td><input type="text" id="sequence-interval" name="sequence_interval" value="0.2"></td>
+</tr>
+<tr>
+	<td>Define the winning return:</td>
+	<td><input type="text" id="winning-return" name="winning_return" value="1" size="2" maxlength="2">:1</td>
 </tr>
 <tr>
 	<td colspan="2">
@@ -90,19 +145,15 @@ function spins() {
 <tr>
 	<td colspan="2" align="right">
 		<input type="button" name="go" value="Go!" onClick="javascript:do_sequence();">
-		<br><br>
+
+<br>
+<br>
 	</td>
 </tr>
 <tr>
 	<td colspan="2">
 		<b>next bet:</b>
 		<div id="next-bet" style="float:right;" class="div-o"></div><br><br>
-	</td>
-</tr>
-<tr>
-	<td colspan="2">
-		<b>spins:</b>
-		<div id="spins" style="float:right;" class="div-o"></div><br><br>
 	</td>
 </tr>
 <tr>
@@ -117,6 +168,17 @@ function spins() {
 	<td colspan="2">
 		<div id="sequence-stream" class="div-o"></div>
 	</td>
+</tr>
+</table>
+
+<br>
+<br>
+
+<table>
+<tr>
+	<td colspan="2"><b>spins:</b><div id="spins" style="float:right;" class="div-o">0</div></td>
+	<td colspan="2"><b>wins:</b><div id="total-wins" style="float:right;" class="div-o">0</div></td>
+	<td colspan="2"><b>losses:</b><div id="total-losses" style="float:right;" class="div-o">0</div></td>
 </tr>
 </table>
 
